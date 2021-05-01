5e7fd220ecaa60c9645565f541bd1cb8ff8b3576
6msadegh <6msadeghalimsa@gmail.com>
2021-05-01 06:48:51 +0430

    Add Test Bench

---
+++ TB.sv
@@ -0,0 +1,14 @@
+`timescale 1ns/1ns
+module TB();
+  reg reset,clk = 0;
+  ARM UUT(clk,reset);
+  always
+   #100 clk=~clk;
+  initial begin
+    reset=1;
+    #200
+    reset=0;
+    #2000;
+    $stop;
+  end
+endmodule 
