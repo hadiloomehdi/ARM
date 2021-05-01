18242c6f87a674d62a73c4d8671fd5872a07ce0f
6msadegh <6msadeghalimsa@gmail.com>
2021-05-01 06:39:07 +0430

    Add EX_Stage

---
+++ EX_Stage.sv
@@ -0,0 +1,12 @@
+`timescale 1ns/1ns
+module EX_Stage(input clk,reset,
+                input [31:0] PC_in,
+                output [31:0] PC);
+                
+        assign  PC = PC_in;
+                
+endmodule
+
+
+
+
