62658e9d170ae90a21932bef1ff852589c7f2516
6msadegh <6msadeghalimsa@gmail.com>
2021-05-11 17:17:22 +0430

    Complete MEM_Stage

--- MEM_Stage.sv
+++ MEM_Stage.sv
@@ -1,8 +1,14 @@
 `timescale 1ns/1ns
-module MEM_Stage(input clk,reset,
-                input [31:0] PC_in,
-                output [31:0] PC);
-                
-        assign  PC = PC_in;
-                
+module MEM_Stage(
+  input clk,reset,MEMread,MEMwrite,
+  input[31:0]address,data,
+  
+  output[31:0] MEM_result
+                );
+              
+  
+  Data_Mem memmory(.Address(address) , .Write_data(data),
+                    .Mem_Read(MEMread),.Mem_Write(MEMwrite),
+                    .clk(clk),.reset(reset),.Read_Data(MEM_result));
+    
 endmodule
