ad5d974f4a6ec51d8923599ef7c6f33d710fab31
6msadegh <6msadeghalimsa@gmail.com>
2021-05-11 17:23:19 +0430

    Complete WB_Stage

--- WB_Stage.sv
+++ WB_Stage.sv
@@ -1,8 +1,10 @@
 `timescale 1ns/1ns
-module WB_Stage(input clk,reset,
-                input [31:0] PC_in,
-                output [31:0] PC);
-                
-        assign  PC = PC_in;
+module WB_Stage(
+  input[31:0] ALU_result,MEM_result,
+  input MEM_R_en,
+
+  output [31:0] out);
+  
+   MUX2to1_32bit wb_mux(.a(ALU_result),.b(MEM_result),.sel(MEM_R_en),.out(out));
                 
 endmodule
