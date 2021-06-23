4fb55765f05eeab463675cdc9574939a0c9c02e4
6msadegh <6msadeghalimsa@gmail.com>
2021-06-24 03:53:59 +0430

    Add SRAM to TB

--- TB.sv
+++ TB.sv
@@ -2,15 +2,28 @@
 module TB();
   reg reset,clk = 0;
   reg Forward_en = 0;
-  ARM UUT(clk,reset,Forward_en);
+  reg SRAM_clk = 0;
+  
+  wire SRAM_WE_N;
+  wire [16:0] SRAM_ADDR;
+  wire [31:0] SRAM_DQ;
+  
+  ARM ARM_UUT(.clk(clk),.reset(reset),.Forward_en(Forward_en),
+          .SRAM_WE_N(SRAM_WE_N),
+          .SRAM_ADDR(SRAM_ADDR),.SRAM_DQ(SRAM_DQ));
+              
+  SRAM SRAM_UUT(.clk(SRAM_clk),.reset(reset),
+                .SRAM_WE_N(SRAM_WE_N),
+                .SRAM_ADDR(SRAM_ADDR),.SRAM_DQ(SRAM_DQ));
 
-  always
-   #100 clk=~clk;
+  always #10 clk=~clk;
+  always #20 SRAM_clk = ~SRAM_clk;
+  
   initial begin
     reset=1;
     #200
     reset=0;
-    #100000;
+    #13000;
     $stop;
   end
 endmodule 
